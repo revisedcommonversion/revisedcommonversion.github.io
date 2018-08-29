722fb0d7cbcb6ab4c32cf43f6f8cbc86037777ae
revisedcommonversion <william@masopust.us>
2018-08-29 18:29:22 -0500

    Working on 404 error page improvements.

---
+++ js/error-page.js
@@ -0,0 +1,11 @@
+var old_url = document.location.href;
+var new_url = "";
+var split_url = old_url.split("/");
+
+document.getElementById("old-url").innerHTML = document.location.href;
+if (split_url[3] === "blog") {
+  document.getElementById("new-url").innerHTML = '<a href="http://www.revisedcommonversion.com/pages/articles.html">http://www.revisedcommonversion.com/pages/articles.html</a>';
+}
+else {
+  document.getElementById("new-url").innerHTML = '<a href="http://www.revisedcommonversion.com">http://www.revisedcommonversion.com</a>';
+}
