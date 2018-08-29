956ac016723fcd902fb5620d1eb9b9112d93b0e6
revisedcommonversion <william@masopust.us>
2018-08-29 18:34:09 -0500

    Working on 404 error page improvements.

--- js/error-page.js
+++ js/error-page.js
@@ -6,6 +6,9 @@ document.getElementById("old-url").innerHTML = document.location.href;
 if (split_url[3] === "blog") {
   document.getElementById("new-url").innerHTML = '<a href="http://www.revisedcommonversion.com/pages/articles.html">http://www.revisedcommonversion.com/pages/articles.html</a>';
 }
+else if (split_url[3] === "devotional") {
+  document.getElementById("new-url").innerHTML = '<a href="http://www.revisedcommonversion.com/pages/devotional/' + split_url[4] + '.html">http://www.revisedcommonversion.com/pages/devotional/' + split_url[4] + '.html</a>';
+}
 else {
   document.getElementById("new-url").innerHTML = '<a href="http://www.revisedcommonversion.com">http://www.revisedcommonversion.com</a>';
 }
