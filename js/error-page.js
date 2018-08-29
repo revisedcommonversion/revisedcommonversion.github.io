1d64552b57b3f620c927b4a3164d689b97c1a8af
revisedcommonversion <william@masopust.us>
2018-08-29 18:53:40 -0500

    Working on the 404 error page JavaScript file.

--- js/error-page.js
+++ js/error-page.js
@@ -3,7 +3,8 @@ var new_url = "";
 var split_url = old_url.split("/");
 
 document.getElementById("old-url").innerHTML = document.location.href;
-if (split_url[3] === "blog") {
+
+/*if (split_url[3] === "blog") {
   document.getElementById("new-url").innerHTML = '<a href="http://www.revisedcommonversion.com/pages/articles.html">http://www.revisedcommonversion.com/pages/articles.html</a>';
 }
 else if (split_url[3] === "devotional") {
@@ -11,4 +12,17 @@ else if (split_url[3] === "devotional") {
 }
 else {
   document.getElementById("new-url").innerHTML = '<a href="http://www.revisedcommonversion.com">http://www.revisedcommonversion.com</a>';
+}*/
+
+switch (split_url[3]) {
+  case "blog":
+    new_url = '<a href="http://www.revisedcommonversion.com/pages/articles.html">http://www.revisedcommonversion.com/pages/articles.html</a>';
+    break;
+  case "devotional":
+    new_url = '<a href="http://www.revisedcommonversion.com/pages/devotional/' + split_url[4] + '.html">http://www.revisedcommonversion.com/pages/devotional/' + split_url[4] + '.html</a>';
+    break;
+  default:
+    new_url = "No page to suggest. Try the suggestions below.";
 }
+
+document.getElementById("new-url").innerHTML = new_url;
