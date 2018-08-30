b5b7a47b83c53e321e2034cff81ffb770a3525bb
revisedcommonversion <william@masopust.us>
2018-08-30 16:37:23 -0500

    Completed the Search page for now. Waiting for search engines to update their respective index for better results. For now, the search page is usable, but not always helpful.

---
+++ js/search-page.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+  $("#search-query").click(function(){
+    $("#search-query").val("");
+  });
+  $("#duck-search-button").click(function(){
+    var duck_url = "https://duckduckgo.com/?q=" + $("#search-query").val() + "+site%3Awww.revisedcommonversion.com&ia=web";
+    window.location = duck_url;
+  });
+  $("#google-search-button").click(function(){
+    var search_url = "https://www.revisedcommonversion.com/pages/search.html?q=" + $("#search-query").val();
+    window.location = search_url;
+  });
+  $(document).keypress(
+    function(event){
+      if (event.which == '13') {
+        event.preventDefault();
+      }
+  });
+});
+
+/*
+  Example search links:
+    DuckDuckGo: https://duckduckgo.com/?q=propitiation+site%3Awww.revisedcommonversion.com&ia=web
+    Google: https://www.google.com/search?q=site%3Awww.revisedcommonversion.com+propitiation
+*/
