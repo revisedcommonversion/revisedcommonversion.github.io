5067a88dfee72fd78ad4710051dd80ddb6be9d2e
revisedcommonversion <william@masopust.us>
2018-08-30 17:21:35 -0500

    Removed unnecessary comments from the messages.js file.

--- js/messages.js
+++ js/messages.js
@@ -2,7 +2,4 @@ $(document).ready(function(){
   $(".dismiss").click(function(){
     $(this).parent().fadeOut();
   });
-  //$(".dismiss").on("click", function () {
-  //  $(this).parents("p").fadeOut();
-  //});
 });
