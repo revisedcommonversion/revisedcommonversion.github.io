78d6a7d377afd44c4c8aa1739d103fcf9f8460a9
revisedcommonversion <william@masopust.us>
2018-08-29 19:13:29 -0500

    Working on the 404 eror page.

--- js/error-page.js
+++ js/error-page.js
@@ -1,21 +1,26 @@
-var old_url = document.location.href;
-var new_url = "";
-var split_url = old_url.split("/");
+(function() {
+  var old_url = document.location.href;
+  var new_url = "";
+  var split_url = old_url.split("/");
 
-document.getElementById("old-url").innerHTML = document.location.href;
+  document.getElementById("old-url").innerHTML = document.location.href;
 
-switch (split_url[3]) {
-  case "blog":
-    new_url = '<a href="http://www.revisedcommonversion.com/pages/articles.html">http://www.revisedcommonversion.com/pages/articles.html</a>';
-    break;
-  case "devotional":
-    new_url = '<a href="http://www.revisedcommonversion.com/pages/devotional/' + split_url[4] + '.html">http://www.revisedcommonversion.com/pages/devotional/' + split_url[4] + '.html</a>';
-    break;
-  case "genesis":
-    new_url = '<a href="http://www.revisedcommonversion.com/text/genesis/' + split_url[4] + '.html">http://www.revisedcommonversion.com/text/genesis/' + split_url[4] + '.html</a>';
-    break;
-  default:
-    new_url = "No page to suggest. Try the suggestions below.";
-}
+  switch (split_url[3]) {
+    case "blog":
+      new_url = '<a href="http://www.revisedcommonversion.com/pages/articles.html">http://www.revisedcommonversion.com/pages/articles.html</a>';
+      break;
+    case "devotional":
+      new_url = '<a href="http://www.revisedcommonversion.com/pages/devotional/' + split_url[4] + '.html">http://www.revisedcommonversion.com/pages/devotional/' + split_url[4] + '.html</a>';
+      break;
+    case "genesis":
+      new_url = '<a href="http://www.revisedcommonversion.com/text/genesis/' + split_url[4] + '.html">http://www.revisedcommonversion.com/text/genesis/' + split_url[4] + '.html</a>';
+      break;
+      case "exodus":
+      new_url = '<a href="http://www.revisedcommonversion.com/text/exodus/' + split_url[4] + '.html">http://www.revisedcommonversion.com/text/exodus/' + split_url[4] + '.html</a>';
+      break;
+    default:
+      new_url = "No page to suggest. Try the suggestions below.";
+  }
 
-document.getElementById("new-url").innerHTML = new_url;
+  document.getElementById("new-url").innerHTML = new_url;
+})();
