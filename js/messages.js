c49d08cb8874e6a173b6db3cbbb74bc5787bbb8c
revisedcommonversion <william@masopust.us>
2018-08-30 17:18:17 -0500

    Added a dismiss option for messages on the Home page.

---
+++ js/messages.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $(".dismiss").click(function(){
+    $(this).parent().fadeOut();
+  });
+  //$(".dismiss").on("click", function () {
+  //  $(this).parents("p").fadeOut();
+  //});
+});
