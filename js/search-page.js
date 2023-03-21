111f242719279444e184a768c15f639d0b312c9c
revisedcommonversion <info@revisedcommonversion.com>
2023-03-21 13:43:54 -0500

    General cleanup and updates in the pages directory.

--- js/search-page.js
+++ js/search-page.js
@@ -11,15 +11,6 @@ $(document).ready(function(){
       alert("Please provide a search query.");
     }
   });
-  $("#google-search-button").click(function(){
-    if ($("#search-query").val().length > 0) {
-      var search_url = "https://www.revisedcommonversion.com/pages/results.html?q=" + $("#search-query").val();
-      window.location = search_url;
-    }
-    else {
-      alert("Please provide a search query.");
-    }
-  });
   $(document).keypress(
     function(event){
       if (event.which == '13') {
@@ -31,5 +22,4 @@ $(document).ready(function(){
 /*
   Example search links:
     DuckDuckGo: https://duckduckgo.com/?q=propitiation+site%3Awww.revisedcommonversion.com&ia=web
-    Google: https://www.google.com/search?q=site%3Awww.revisedcommonversion.com+propitiation
 */
