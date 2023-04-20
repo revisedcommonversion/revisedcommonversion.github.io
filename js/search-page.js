5a506f53eb9dbac8129b41efdb95c1b37a97ce06
revisedcommonversion <info@revisedcommonversion.com>
2023-04-20 10:44:55 -0500

    Added Bing as a site search option.

--- js/search-page.js
+++ js/search-page.js
@@ -2,6 +2,15 @@ $(document).ready(function(){
   $("#search-query").click(function(){
     $("#search-query").val("");
   });
+  $("#bing-search-button").click(function(){
+    if ($("#search-query").val().length > 0) {
+      var bing_url = "https://www.bing.com/search?q=" + $("#search-query").val() + "+site%3Arevisedcommonversion.com";
+      window.location = bing_url;
+    }
+    else {
+      alert("Please provide a search query.");
+    }
+  });
   $("#duck-search-button").click(function(){
     if ($("#search-query").val().length > 0) {
       var duck_url = "https://duckduckgo.com/?q=" + $("#search-query").val() + "+site%3Awww.revisedcommonversion.com&ia=web";
@@ -30,6 +39,7 @@ $(document).ready(function(){
 
 /*
   Example search links:
+    Bing: https://www.bing.com/search?q=propitiation+site%3Arevisedcommonversion.com
     DuckDuckGo: https://duckduckgo.com/?q=propitiation+site%3Awww.revisedcommonversion.com&ia=web
-    Ecosia: https://www.ecosia.org/search?q=scepter%20site%3Arevisedcommonversion.com
+    Ecosia: https://www.ecosia.org/search?q=propitiation%20site%3Arevisedcommonversion.com
 */
